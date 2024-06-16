e15f70df70fae7b3d03d71bfe183cc9a8b3061e5
heathernfran <heathernfran@gmail.com>
2024-06-16 11:26:42 -0700

    Add title and back button to photo details

--- src/components/PhotoDetails.tsx
+++ src/components/PhotoDetails.tsx
@@ -1,13 +1,20 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { buildImageUrl } from "../lib/utilities";
 
 export default function PhotoDetails() {
   const location = useLocation();
+  const navigate = useNavigate();
   const { photo } = location.state;
 
   return (
     <div>
-      <img src={buildImageUrl(photo, "b")} alt={photo.title} />
+      <div>
+        <button onClick={() => navigate(-1)}>Back</button>
+      </div>
+      <div>
+        <h1>{photo.title}</h1>
+        <img src={buildImageUrl(photo, "b")} alt={photo.title} />
+      </div>
     </div>
   );
 }
