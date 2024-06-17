4d5a96d5594695e5cf8c212e0eaa6c329433baa2
heathernfran <heathernfran@gmail.com>
2024-06-16 18:46:25 -0700

    Fix re-rendering Photos component by using form state for search term, instead of updating state on change

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,35 +1,27 @@
 import { useState } from "react";
-import type { ChangeEvent, FormEvent } from "react";
+import type { FormEvent } from "react";
 import usePhotos from "../hooks/usePhotos";
 import { getSearchResults } from "../lib/api";
 
 export default function Search() {
   const { state, dispatch } = usePhotos();
-  const { pageNumber, searchTerm } = state;
+  const { pageNumber } = state;
   const [error, setError] = useState("");
-  let timeoutId: ReturnType<typeof setTimeout>;
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      const nextSearchTerm = event.target.value;
-      dispatch({ type: "UPDATE_SEARCH_TERM", searchTerm: nextSearchTerm });
-    }, 100);
-  }
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setError("");
 
-    if (!searchTerm.length) {
+    const formData = new FormData(event.target as HTMLFormElement);
+    const nextSearchTerm = formData.get("search")?.toString();
+    if (!nextSearchTerm) {
       setError("Please enter a search term");
       return;
     }
 
     try {
-      const results = await getSearchResults(searchTerm, pageNumber);
+      const results = await getSearchResults(nextSearchTerm, pageNumber);
+      dispatch({ type: "UPDATE_SEARCH_TERM", searchTerm: nextSearchTerm });
       dispatch({ type: "RESET_SEARCH" });
       dispatch({ type: "ADD_PHOTOS", photos: results.photo });
       dispatch({ type: "UPDATE_TOTAL_PAGES", totalPages: results.pages });
@@ -40,10 +32,8 @@ export default function Search() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>
-        Search for photos
-        <input onChange={handleChange} type="text" />
-      </label>
+      <label htmlFor="search">Search for photos</label>
+      <input name="search" type="text" />
       <button type="submit">Search</button>
       {error && <div>Error in search: {error}</div>}
     </form>
